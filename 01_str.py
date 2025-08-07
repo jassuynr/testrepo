d9938c2dbcf4bb290a220f0534314f851a13e28b
jassuynr <jasdeepsingh5234@gmail.com>
2025-08-07 12:31:06 +0530

    Add files via upload

---
+++ 01_str.py
@@ -0,0 +1,7 @@
+name="jassi"# string is immutable we cant change existing string
+name2=name[0:3]# slicing do by indexing 0 se 2 tk
+no=len(name)
+print(no)
+print(name2)
+character="harry"
+print(character[0])
